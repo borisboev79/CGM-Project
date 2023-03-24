ac4c24c00fe35d12a856a0857a9d13ba2aec89a3
Boris Boev <boris.boev@gmail.com>
2023-03-24 22:31:34 +0200

    One more test

--- src/test/java/cgm/controller/GroupControllerIT.java
+++ src/test/java/cgm/controller/GroupControllerIT.java
@@ -1,11 +1,11 @@
 package cgm.controller;
 
 import cgm.model.dto.GroupAddDto;
-import cgm.model.entity.CruiseLine;
-import cgm.model.entity.Ship;
+import cgm.model.entity.*;
+import cgm.model.enums.BranchCode;
+import cgm.model.enums.Role;
 import cgm.model.enums.Transportation;
-import cgm.repository.CruiseLineRepository;
-import cgm.repository.ShipRepository;
+import cgm.repository.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,7 +13,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -29,6 +32,18 @@ public class GroupControllerIT {
 
     @Autowired
     private CruiseLineRepository cruiseLineRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private BranchRepository branchRepository;
+
+    @Autowired
+    private GroupRepository groupRepository;
 
 
     @Autowired
@@ -47,7 +62,7 @@ public class GroupControllerIT {
     }
 
     @Test
-    @WithMockUser(username = "admin", roles={"USER", "MANAGER","ADMIN"})
+    @WithMockUser(username = "admin", roles = {"USER", "MANAGER", "ADMIN"})
     public void testGetGroupAdd() throws Exception {
 
         mockMvc.perform(get("/groups/add"))
@@ -57,7 +72,7 @@ public class GroupControllerIT {
     }
 
     @Test
-    @WithMockUser(username = "admin", roles={"ADMIN"})
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
     public void testAddGroup() throws Exception {
 
         CruiseLine cruiseLine = CruiseLine.builder().name("MSC Cruises").build();
@@ -82,13 +97,13 @@ public class GroupControllerIT {
         group.setTransportation(Transportation.BUS);
 
         mockMvc.perform(post("/groups/add").flashAttr("groupAddDto", group)
-                .with(csrf()))
+                        .with(csrf()))
                 .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/cabins/add/1"));
+                .andExpect(redirectedUrl("/cabins/add/3"));
     }
 
     @Test
-    @WithMockUser(username = "admin", roles={"USER", "MANAGER","ADMIN"})
+    @WithMockUser(username = "admin", roles = {"USER", "MANAGER", "ADMIN"})
     public void testGetDetails() throws Exception {
 
 
@@ -99,7 +114,7 @@ public class GroupControllerIT {
     }
 
     @Test
-    @WithMockUser(username = "admin", roles={"USER", "MANAGER","ADMIN"})
+    @WithMockUser(username = "admin", roles = {"USER", "MANAGER", "ADMIN"})
     public void testGetGroupsDelete() throws Exception {
 
         mockMvc.perform(get("/groups/delete"))
@@ -108,14 +123,7 @@ public class GroupControllerIT {
                 .andExpect(view().name("groups-delete"));
     }
 
-    @Test
-    @WithMockUser(username = "admin", roles={"ADMIN"})
-    public void testDeleteGroup() throws Exception {
 
-        mockMvc.perform(get("/groups/delete/1"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/groups/all"));
-    }
 
     @Test
     public void testDeleteGroupFail() throws Exception {
@@ -123,5 +131,73 @@ public class GroupControllerIT {
         mockMvc.perform(get("/groups/delete/1"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("http://localhost/auth/login"));
-}
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    public void testDeleteGroup() throws Exception {
+
+                RoleEntity role = new RoleEntity();
+        role.setRole(Role.ADMIN);
+        role.setDescription("Admin");
+
+        roleRepository.saveAndFlush(role);
+
+        BranchEntity branch = BranchEntity.builder()
+                .name("Blagoevgrad")
+                .code(BranchCode.BLGD)
+                .address("Blagoevgrad")
+                .email("blgd@sofia.bg")
+                .build();
+
+        branchRepository.saveAndFlush(branch);
+
+
+        UserEntity user = UserEntity.builder()
+                .username("petarcho")
+                .password("topsecret")
+                .firstName("Petar")
+                .lastName("Petrov")
+                .roles(List.of(role))
+                .branch(branch)
+                .build();
+
+        userRepository.saveAndFlush(user);
+
+
+        CruiseLine cruiseLine = CruiseLine.builder().name("Costa Cruises").build();
+
+        cruiseLineRepository.saveAndFlush(cruiseLine);
+
+        Ship ship = Ship.builder()
+                .name("Costa Cosmia")
+                .cruiseLine(cruiseLine)
+                .build();
+
+        shipRepository.saveAndFlush(ship);
+
+
+        CruiseGroup group2 = CruiseGroup.builder()
+                .name("GGJGJJGJGJGJGJGJG")
+                .duration(7L)
+                .startDate(Instant.now())
+                .endDate(Instant.now().plus(7, ChronoUnit.DAYS))
+                .itinerary("LLKLKLLKLKLKLKLKLKLKLKL")
+                .totalPax(24)
+                .ship(ship)
+                .employee(user)
+                .transportation(Transportation.BUS)
+                .imageUrl("ggogogog.gi/ififi.jpg")
+                .build();
+
+        groupRepository.saveAndFlush(group2);
+
+        group2.setId(2L);
+
+        groupRepository.saveAndFlush(group2);
+
+        mockMvc.perform(get("/groups/delete/2"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/groups/delete"));
+    }
 }
